12652adc2b3a654c6398a7fadc20b00545cea10e
jettinlim <49452311+jettinlim@users.noreply.github.com>
2021-05-16 22:46:07 +0800

    Update decrypt_string.cpp

--- Leetcode算法题/string/decrypt_string.cpp
+++ Leetcode算法题/string/decrypt_string.cpp
@@ -73,7 +73,7 @@ int main()
 {
 	auto t1 = chrono::high_resolution_clock::now();
   
-  string s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#";
+  	string s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#";
 	cout << freqAlphabets_v2(s);
 
 
