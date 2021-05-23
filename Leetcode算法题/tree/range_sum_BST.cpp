e278c9023ca461362625452396fb48db84f01fe2
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-05-23 14:38:52 +0800

    Update range_sum_BST.cpp

--- Leetcode算法题/tree/range_sum_BST.cpp
+++ Leetcode算法题/tree/range_sum_BST.cpp
@@ -18,11 +18,11 @@ public:
         
         if (root == NULL)
             return 0;
-        else if (root->val < low)
+        else if (root->val < low)       //if node value is smaller than low, no need to see the lefts anymore
             return rangeSumBST(root->right, low, high);
-        else if (root->val <= high)
-            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high); 
+        else if (root->val <= high)     
+            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);        //add all the suitable node value in between
         else
-            return rangeSumBST(root->left, low, high);
+            return rangeSumBST(root->left, low, high);      //if node value is higher than high, no need to see the rights anymore
     }
 };
