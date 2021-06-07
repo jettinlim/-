d2238c5aeb50d4801c5f45c7e0cd5ca53bda2aab
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-06-07 23:14:49 +0800

    Update merge_two_BT.cpp

--- Leetcode算法题/tree/merge_two_BT.cpp
+++ Leetcode算法题/tree/merge_two_BT.cpp
@@ -1,16 +1,6 @@
 //https://leetcode.com/problems/merge-two-binary-trees/
 //https://www.geeksforgeeks.org/merge-two-binary-trees-node-sum/#:~:text=Given%20two%20binary%20trees.,the%20node%20of%20new%20tree.
 
-#include <iostream>
-#include <map>
-#include <string>
-#include <chrono>
-#include <vector>
-#include <numeric>
-#include <queue>
-
-using namespace std;
-
 struct TreeNode {
 	int val;
 	TreeNode* left;
@@ -45,32 +35,4 @@ TreeNode* mergeTree(TreeNode* root1, TreeNode* root2) {		//25/5/21		runtime: 93.
 		return root1 ? root1 : root2;
 
 	return root1;
-}
-
-
-int main()
-{
-	auto t1 = chrono::high_resolution_clock::now();
-
-	TreeNode* root1 = new TreeNode(1);
-	root1->left = new TreeNode(3);
-	root1->left->left = new TreeNode(5);
-	root1->left->right = new TreeNode(3);
-	root1->right = new TreeNode(2);
-
-	TreeNode* root2 = new TreeNode(2);
-	root2->left = new TreeNode(1);
-	root2->left->right = new TreeNode(4);
-	root2->left->left = new TreeNode(3);
-	root2->right = new TreeNode(3);
-	root2->right->right = new TreeNode(7);
-
-	cout << travelTree(mergeTree(root1, root2));
-
-
-	auto t2 = chrono::high_resolution_clock::now();
-	auto t3 = chrono::duration_cast<chrono::milliseconds>(t2 - t1);
-	cout << "\n\nRuntime: " << t3.count() << "ms\n";
-
-	return 0;
 }
