3014959b69f617d9163394efa40d41dfab5cebb0
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-06-08 22:41:30 +0800

    Create max_depth_B_tree.cpp

---
+++ Leetcode算法题/tree/max_depth_B_tree.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+//Leetcode 104: Maximum Depth of Binary Tree		8/6/2021		runtime: 29.84%, memory: 12.23%, time: O(n=number of nodes), space: O(h=max height)
+int maxDepth(TreeNode* root, int depth = 0) {
+	if (root == nullptr) return depth;
+	depth++;
+
+	return max(maxDepth(root->right, depth), maxDepth(root->left, depth));
+}
