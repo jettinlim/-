2f199ee653ed19a62c8c64db5cfb52ac57587a08
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-06-14 22:31:15 +0800

    Create validate_BST.cpp

---
+++ Leetcode算法题/tree/validate_BST.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/validate-binary-search-tree/
+
+//Leetcode 98: Validate Binary Search Tree		14/6/2021	time&space: O(n=count of nodes)
+class Solution {
+public:
+  vector<int> v;
+  bool isValidBST(TreeNode* root) {
+    if (!root) return true;
+    bool left = isValidBST(root->left);
+    v.push_back(root->val);
+    if (v.size() >= 2 && v.back() <= v[v.size() - 2]) return false;   //store every nodes value inorderly,then compare with previous value, if not larger then it is not a BST
+    bool right = isValidBST(root->right);
+    return left && right;
+  }
+};
+
+//Leetcode god solution
+bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+    if(!root) return true;
+    if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+        return false;
+    return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+}
