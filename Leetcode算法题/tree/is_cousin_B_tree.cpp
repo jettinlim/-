f654e9c4489d9716eaae44931c4c113806130e2b
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-06-29 00:02:39 +0800

    Create is_cousin_B_tree.cpp

---
+++ Leetcode算法题/tree/is_cousin_B_tree.cpp
@@ -0,0 +1,55 @@
+//https://leetcode.com/problems/cousins-in-binary-tree/
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+//Leetcode 993: Cousins in Binary Tree			28/6/2021		time&space: O(n), vector and stack	
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = x+y;
+        
+        while(!q.empty()){
+            int current_size = q.size();
+            //clear vector
+            vector<int> v = {};
+            for(int i = 0; i < current_size; i++){
+                TreeNode* current = q.front();
+                q.pop();
+                
+                //current root have both left & right node
+                if(current->left && current->right)
+                    //they are siblings, return false
+                    if (sum-current->left->val==current->right->val || sum-current->right->val==current->left->val) return false;
+                
+                if(current->left){
+                    q.push(current->left);
+                    v.push_back(current->left->val);
+                } 
+                if(current->right){
+                    q.push(current->right);
+                    v.push_back(current->right->val);
+                }
+            }
+            
+            if(std::find(v.begin(), v.end(), x) != v.end())
+            {
+              if(std::find(v.begin(), v.end(), y) != v.end())
+                {
+                    //Both value are at the same level
+                    return true;
+                } 
+            } 
+        }
+        
+        return false;
+    }
+};
