bd6bb700ec51c3a5d2c14c2032ba560a41e2324a
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-08-09 00:02:31 +0800

    Update corresponding_node.cpp

--- Leetcode算法题/tree/corresponding_node.cpp
+++ Leetcode算法题/tree/corresponding_node.cpp
@@ -10,12 +10,11 @@ struct TreeNode {
 //Leetcode 1379: Find a Corresponding Node of a Binary Tree in a Clone of That Tree			8/8/2021			time, space: O(h), DFS
 class Solution {
 public:
-    TreeNode* temp;
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        if(!original && !cloned) return cloned;
-        if(cloned->val == target->val) temp = cloned;
-        getTargetCopy(original->left, cloned->left, target);
-        getTargetCopy(original->right, cloned->right, target);
-        return temp;
+        if(!original && !cloned) return NULL;
+        if(cloned->val == target->val) return cloned;
+        TreeNode* l = getTargetCopy(original->left, cloned->left, target);
+        TreeNode* r = getTargetCopy(original->right, cloned->right, target);
+        return l != NULL ? l : r;
     }
 };
