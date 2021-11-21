a65eb7d75248d290618f494577eac9432b81c397
LIM JET TIN <49452311+jettinlim@users.noreply.github.com>
2021-11-21 18:35:20 +0800

    Create toLowerCase.java

---
+++ Leetcode算法题/string/toLowerCase.java
@@ -0,0 +1,15 @@
+//Leetcode 709: To Lower Case
+
+class Solution {
+    public String toLowerCase(String str) {
+        
+        String res = "";
+        
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            res += Character.isLetter(c) && (int)c <= 90 ? (char)((int)c + 32) : c;
+        }
+        
+        return res;
+    }
+}
